b52c9766e09d4eb749c2a35a6b9991db7e080849
Baraa Chrit <baraaadnan01@gmail.com>
2023-08-30 14:33:41 -0400

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-
-// import react from '@vitejs/plugin-react';
-
-// export default {
-//   plugins: [react()],
-//   build: {
-//     rollupOptions: {
-//       external: ['react-dom/client'],
-//     },
-//   },
-// };
